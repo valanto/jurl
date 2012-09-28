6dc31cb7ecb77c55c02ea1910a950656c6ef74ce
valanto@gmail.com <valanto@gmail.com>
2012-09-28 22:49:33 +0300

    Initial files and libraries

--- jurl.js
+++ jurl.js
@@ -1,2 +1,27 @@
-jurl = {};
+var jurl = function(b){
+	var that = this;
+	var base;
+	initialParse(b);
+	
+	
+	
+	that.build = function(){
+		return base;
+	}
+	
+	function initialParse(b){
+		base = b;
+	}
+	
+	return that;
+};
 
+(function( $ ) {
+  $.fn.jurl = function() {
+	if(this.attr("href")){
+	  	return new jurl(this.attr("href"));		
+	}
+	throw "Not href attribute on element: " + input;
+   
+  };
+})( jQuery );

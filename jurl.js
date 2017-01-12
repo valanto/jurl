05a39ecf2f8fecadba36f2e502382665e34077e8
valanto <valanto@gmail.com>
2017-01-12 13:04:55 +0200

    Update jurl.js

--- jurl.js
+++ jurl.js
@@ -1,21 +1,21 @@
 var jurl = function (b) {
 	var that = this;
 	var url = initialParse(b);
-	
+
 	that.addUrlParameter = function(key, index){
 		key=trim(key);
 		if(!isBlank(key)){
 			if(isBlank(index) && isNaN(index)){
-				url.urlParameters.push(key);							
+				url.urlParameters.push(key);
 			} else {
 				if(index < url.urlParameters.length){
-					url.urlParameters.splice(index, 0, key);					
+					url.urlParameters.splice(index, 0, key);
 				}
 			}
 		}
 		return that;
 	};
-	
+
 	that.setQueryParameter = function(key, value){
 		key=trim(key);
 		if(!isBlank(key)){
@@ -26,21 +26,51 @@ var jurl = function (b) {
 		}
 		return that;
 	};
-	
+
 	that.setHashParameter = function(hash){
 		hash=trim(hash);
 		if(isBlank(hash)){
 			url.hashParameter = null;
 		}
 		url.hashParameter=trim(hash);
-		
+
 		return that;
 	};
+
+	that.setHashQueryParameter = function(key, value){
+		key=trim(key);
+		if(!isBlank(key)){
+			url.hashQueryParameters[key] = "";
+			if(!isBlank(value)){
+				url.hashQueryParameters[key] = value;
+			}
+		}
+
+		return that;
+	}
+
+	that.getHashQueryParameter = function(key){
+		key=trim(key);
+		if(isBlank(key) ||!url.hashQueryParameters.hasOwnProperty(key)){
+			return null;
+		}
+		return url.hashQueryParameters[key];
+	};
+
+	that.updateCurrentHash = function(){
+		var hash = that.buildHash();
+		if(hash !== null){
+			document.location.hash = hash;
+		} else {
+            document.location.hash = "";
+        }
+		return that;
+	}
 
 	that.getHashParameter = function(){
 		return url.hashParameter;
 	};
-	
+
 	that.getQueryParameter = function(key){
 		key=trim(key);
 		if(isBlank(key) ||!url.queryParameters.hasOwnProperty(key)){
@@ -48,7 +78,7 @@ var jurl = function (b) {
 		}
 		return url.queryParameters[key];
 	};
-	
+
 	that.getParameterIndex = function(key){
 		key=trim(key);
 		var p;
@@ -63,7 +93,7 @@ var jurl = function (b) {
 	that.getHost = function(key){
 		return url.base;
 	};
-	
+
 	that.removeUrlParameter = function(key){
 		key=trim(key);
 		if(url.urlParameters.indexOf(key) > -1){
@@ -71,7 +101,7 @@ var jurl = function (b) {
 		}
 		return that;
 	};
-	
+
 	that.removeQueryParameter = function(key){
 		key=trim(key);
 		if(url.queryParameters.hasOwnProperty(key)){
@@ -79,13 +109,21 @@ var jurl = function (b) {
 		}
 		return that;
 	};
-	
+
+
+	that.removeHashQueryParameter = function(key){
+		key=trim(key);
+		if(url.hashQueryParameters.hasOwnProperty(key)){
+			delete url.hashQueryParameters[key];
+		}
+		return that;
+	};
+
 	that.build = function () {
 		var urlString = url.base;
 		if (url.urlParameters.length > 0) {
 			urlString += "/" + url.urlParameters.join("/");
 		}
-		
 		var params = [], p;
 		for (p in url.queryParameters) {
 			if (url.queryParameters.hasOwnProperty(p)) {
@@ -98,42 +136,73 @@ var jurl = function (b) {
 			}
 		}
 		if(params.length > 0){
-			urlString += "?" + params.join("&");			
+			urlString += "?" + params.join("&");
 		}
-		
-		if(!isBlank(url.hashParameter)){
-			urlString += "#" + url.hashParameter;
+
+		var hashString = that.buildHash();
+		if(hashString !== null){
+			urlString += "#" + hashString;
 		}
-		
+
 		return urlString;
 	};
-	
+
+	that.buildHash = function(){
+		if(isBlank(url.hashParameter) && Object.keys(url.hashQueryParameters).length === 0){
+			return null;
+		}
+
+		var hashString = "";
+		if(!isBlank(url.hashParameter)){
+			hashString += url.hashParameter;
+		}
+		if(Object.keys(url.hashQueryParameters).length > 0) {
+			var hashParams =[];
+			for (p in url.hashQueryParameters) {
+				if (url.hashQueryParameters.hasOwnProperty(p)) {
+					var paramString = p;
+					var value = url.hashQueryParameters[p];
+					if (!isBlank(value)) {
+						paramString += "=" + value;
+					}
+					hashParams.push(paramString);
+				}
+			}
+			if(hashParams.length > 0){
+				hashString += "?" + hashParams.join("&");
+			}
+		}
+
+		return hashString;
+	};
+
 	function isBlank (string) {
 		return (string === null) || (typeof (string) === "undefined")  || (trim(string) === "");
 	}
-	
+
 	function trim(string){
 		if((string === null) || (typeof (string) === "undefined")) {
 			return string;
-		} 
+		}
 		string = string + "";
 		return string.replace(/(^\s*)|(\s*$)/g, "");
 	}
-	
+
 	function initialParse (b) {
-		var urlRegex = /^((((file|gopher|news|nntp|telnet|http|ftp|https|ftps|sftp):\/\/)?(www\.)?([a-zA-Z0-9\-\.]+(\.[a-zA-Z]{2,3})?(:[a-zA-Z0-9]*)?))(\/[a-zA-Z0-9\-_\/]*)?)(\?([a-zA-Z0-9\-_&=%.\/]*))?(#([a-zA-Z0-9\-_&=\/]*))?$/;
+		var urlRegex = /^((((file|gopher|news|nntp|telnet|http|ftp|https|ftps|sftp):\/\/)?(www\.)?([a-zA-Z0-9\-\.]+(\.[a-zA-Z]{2,3})?(:[a-zA-Z0-9]*)?))?(\/[%.a-zA-Z0-9\-_\/]*)?)(\?([a-zA-Z0-9\-_&=%.\/]*))?(#([a-zA-Z0-9\-\/]*)(\?([a-zA-Z0-9\-_&=\./|+%]*))?)?$/;
 		var match = urlRegex.exec(b);
 		if(match < 3){
 			return "";
 		}
 		return {
-			base: match[2],
+			base: typeof(match[2]) === "undefined" ? "" : match[2] ,
 			urlParameters: parseUrlParams(match[9]),
 			queryParameters: parseQueryParams(match[11]),
-			hashParameter: match[13]
+			hashParameter: match[13],
+			hashQueryParameters: parseQueryParams(match[15])
 		}
 	}
-	
+
 	function parseQueryParams (queryString) {
 		if (isBlank(queryString)) {
 			return {};
@@ -150,7 +219,7 @@ var jurl = function (b) {
 		}
 		return params;
 	}
-	
+
 	function parseUrlParams (urlString) {
 		if (isBlank(urlString)) {
 			return [];
@@ -159,22 +228,22 @@ var jurl = function (b) {
 		var splitParams = urlString.split("/");
 		var s;
 		for (s=0; s < splitParams.length; s+=1) {
-			if(!isBlank(splitParams[s])){				
+			if(!isBlank(splitParams[s])){
 				params.push(splitParams[s]);
 			}
 		}
 		return params;
 	}
-	
+
 	return that;
 };
 
 (function( $ ) {
   $.fn.jurl = function() {
 	if(this.attr("href")){
-	  	return new jurl(this.attr("href"));		
+	  	return new jurl(this.attr("href"));
 	}
 	throw "Not href attribute on element: " + input;
-   
+
   };
 })( jQuery );
